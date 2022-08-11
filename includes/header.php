eba2fb78fceb53ca8d6fa9b0fc5a3387779ccb3d
BioGator <104639737+BioGator@users.noreply.github.com>
2022-08-11 16:30:31 -0300

    Update "left logo" to BioGator Logo v2

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
 		<td id="header" colspan="3">
 			<div style="clear:both; width:100%; height:170px; border-bottom:1px solid #000000;">
 				<div style="float:left">
-					<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/left_logo.jpg" style="margin:0px 30px;width:130px" />
+					<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/biogator_logo_v2.2.png" style="margin:0px 30px;width:130px" />
 				</div>
 				<div style="margin-left: 40px; color: #fff; font-family: 'Mate', serif; letter-spacing: 1px; text-shadow: 0 0 7px rgba(0,0,0,0.5);">
 					<div style="margin-top:30px; font-size:60px; line-height:48px;">
