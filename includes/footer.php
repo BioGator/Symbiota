c69cf77ea38b4dff80504e6e862011b1aa46551c
BioGator <104639737+BioGator@users.noreply.github.com>
2022-08-15 17:59:12 -0300

    Update footer.php
    Replace NSF comment to: "This project made possible through support from the UF Planning, Design & Construction and the UF Office of Sustainability"

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@
 				<a href="https://facilities.ufl.edu" target="_blank" title="iDigBio"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/BusinessAffairs-PlanningDesignConstruction-VECTOR-RGB.png" style="width:200px" /></a>
 				<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" style="width:180px" /></a>
 			</p>
-			<div style="text-align:center;margin-bottom:15px">This project made possible by National Science Foundation Awards: <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=" target="_blank">#------</a></div>
+			<div style="text-align:center;margin-bottom:15px">This project made possible through support from the UF Planning, Design & Construction and the UF Office of Sustainability </div>
 			<div style="text-align:center;margin-bottom:15px">
 				Powered by <a href="https://symbiota.org/" target="_blank">Symbiota</a>.
 			</div>
