3f7d867cd8c8e44a106bd0961665027faf27d0b1
Greg Post <98365364+GregPost-ASU@users.noreply.github.com>
2022-08-15 10:19:07 -0500

    Update footer.php
    logo adjustment so that UF logos are approximately same height.

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 	<tr>
 		<td id="footer" colspan="3">
 			<p>
-				<a href="https://sustainable.ufl.edu" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/BusinessAffairs-Sustainability-VECTOR-RGB.png" style="width:200px" /></a>
+				<a href="https://sustainable.ufl.edu" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/BusinessAffairs-Sustainability-VECTOR-RGB.png" style="width:320px" /></a>
 				<a href="https://facilities.ufl.edu" target="_blank" title="iDigBio"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/BusinessAffairs-PlanningDesignConstruction-VECTOR-RGB.png" style="width:200px" /></a>
 				<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" style="width:180px" /></a>
 			</p>
