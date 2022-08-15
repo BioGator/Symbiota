55394e4f2bb9e62b28e912bd9bee888a99299c66
Greg Post <98365364+GregPost-ASU@users.noreply.github.com>
2022-08-15 10:11:46 -0500

    Update footer.php
    logo size adjustment.

--- includes/footer.php
+++ includes/footer.php
@@ -3,8 +3,8 @@
 	<tr>
 		<td id="footer" colspan="3">
 			<p>
-				<a href="https://sustainable.ufl.edu" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/BusinessAffairs-Sustainability-VECTOR-RGB.png" style="width:150px" /></a>
-				<a href="https://facilities.ufl.edu" target="_blank" title="iDigBio"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/BusinessAffairs-PlanningDesignConstruction-VECTOR-RGB.png" style="width:150px" /></a>
+				<a href="https://sustainable.ufl.edu" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/BusinessAffairs-Sustainability-VECTOR-RGB.png" style="width:180px" /></a>
+				<a href="https://facilities.ufl.edu" target="_blank" title="iDigBio"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/BusinessAffairs-PlanningDesignConstruction-VECTOR-RGB.png" style="width:180px" /></a>
 				<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" style="width:180px" /></a>
 			</p>
 			<div style="text-align:center;margin-bottom:15px">This project made possible by National Science Foundation Awards: <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=" target="_blank">#------</a></div>
