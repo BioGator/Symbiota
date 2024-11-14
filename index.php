d6d38e113e5af7b6cfa825ce3189d4e66ab2b089
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-11-14 09:09:28 -0800

    Change wording of taxon search

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ header('Content-Type: text/html; charset=' . $CHARSET);
 		<div id="quicksearchdiv">
 			<!-- -------------------------QUICK SEARCH SETTINGS--------------------------------------- -->
 			<form name="quicksearch" id="quicksearch" action="<?php echo $CLIENT_ROOT; ?>/taxa/index.php" method="get" onsubmit="return verifyQuickSearch(this);">
-				<div id="quicksearchtext" ><?php echo (isset($LANG['QSEARCH_SEARCH'])?$LANG['QSEARCH_SEARCH']:'Taxon Search'); ?></div>
+				<div id="quicksearchtext" ><?= "Search by Scientific Name" ?></div>
 				<input id="taxa" type="text" name="taxon" />
 				<button name="formsubmit"  id="quicksearchbutton" type="submit" value="Search Terms"><?php echo (isset($LANG['QSEARCH_SEARCH_BUTTON'])?$LANG['QSEARCH_SEARCH_BUTTON']:'Search'); ?></button>
 			</form>
